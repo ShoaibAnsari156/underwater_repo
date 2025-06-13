058fa2e1fee7a830d6d2e0a20a97e8bed1e54cd2
ZabirAnsari1992 <ryzen53600pc@gmail.com>
2025-06-13 21:02:40 +0530

    dataset

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,18 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>
-        <ul>
-            <li>
-                <Link to='/'>Home</Link>
-            </li>
-            <li>
-                <Link to='about'>About</Link>
-            </li>
-        </ul>
+    <div className='flex justify-between bg-cyan-800 text-white h-auto p-2 border-2 border-black '>
+      <ul className='font-bold flex space-x-10'>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='about'>About</Link>
+        </li>
+      </ul>
+      <div className=''>
+        <input type="search" placeholder="Type Search" className='p-1 border-2 border-black rounded-2xl' />
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
 const Home = () => {
+  const [fishData, setFishData] = useState([]);
+  async function getData() {
+    try {
+      let response = await axios.get("http://localhost:3001/fishData")
+      setFishData(response.data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <div>
-        Home
+      {
+        fishData.map((fish,idx) => (
+          <div key={idx}>
+            <img src={fish.image} alt="" />
+            <p>{fish.name}</p>
+            <p>{fish.description}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
